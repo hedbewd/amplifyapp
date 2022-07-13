1269556f79dc0b2aa8b22add4e053a4a70306f19
unknown <hedbewd>
2022-07-13 14:02:01 +0900

    added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,16 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyappcab61a01": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        },
+        "userPoolGroups": {
+            "managerGroupRole": "string"
+        }
+    }
+}
